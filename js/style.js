3e3d00d9d6def1b5e29953b6ad9fc9f11419738c
Mouradpro08 <110630375+Mouradpro08@users.noreply.github.com>
2022-08-07 13:14:12 +0200

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,13 @@
+let img = document.querySelector('.image1');
+
+let cont = document.querySelector('.contenair');
+
+function  change(iphon13) {
+
+    img.src = iphon13;
+    
+}
+
+function colors(color) {
+   cont.style.background =  color;
+}
